e804d1037d0eafee82132797dfef7825566b1954
Raga Markely <lmarkely@alum.mit.edu>
2017-07-05 22:28:51 -0400

    Add audit_street_name.py

---
+++ audit_street_name.py
@@ -0,0 +1,124 @@
+'''
+This code fixes the street names in OSM file.
+The function takes a string with street name as an argument and returns the fixed name
+There are four major types of errors:
+1. Inconsistency: abbreviation, capitalization, punctuation
+2. Missing the word 'Jalan', Indonesian of 'Street'.
+3. Inclusion of city and province names
+4. Using English word, 'Road'
+Reference: this code was modified from 'Case study: OpenStreetMap Data[SQL] Quiz 10'
+'''
+import csv
+import codecs
+import re
+import xml.etree.cElementTree as ET
+import pprint
+import cerberus
+import schema
+from collections import Counter,defaultdict
+OSMFILE = 'bali.osm'
+
+mapping_street = {  'Jl.': 'Jalan',
+                    'Jl': 'Jalan',
+                    'Jln': 'Jalan',
+                    'Jln.': 'Jalan',
+                    'Jl,': 'Jalan',
+                    'JL.': 'Jalan',
+                    'JLN.': 'Jalan',
+                    'jalan': 'Jalan',
+                    'JL': 'Jalan',
+                    'JALAN': 'Jalan',
+                    'Jalan.': 'Jalan'}
+
+def is_street_name(elem):
+    '''
+    #Check if the element contains a street name.
+    '''
+    return (elem.attrib['k'] == 'addr:street')
+
+def audit_street_type(street_types, street_name):
+    '''
+    Classify the types of errors and store in a dictionary.
+    '''
+    if street_name.split(' ')[0] != 'Jalan':
+        if street_name.split(' ')[0] in mapping_street.keys():
+            street_types[street_name.split(' ')[0]].add(street_name)
+        elif len(street_name.split(',')) > 1 and street_name.split(',')[1][:4] == ' JL.':
+            street_types['JL._invert'].add(street_name)
+        elif street_name.split(' ')[0][:3] == 'Jl.':
+            street_types['Jl_no_space'].add(street_name)
+        elif len(street_name.split(',')) > 1:
+            if street_name.split(',')[0][:5] == 'Jalan':
+                street_types['extra'].add(street_name)
+            else:
+                street_types['extra_no_Jalan'].add(street_name)
+        elif street_name[-4:] == 'Road':
+            street_types['Road'].add(street_name)
+        else:
+            street_types['None'].add(street_name)
+
+def update_name(name, mapping_street):
+    '''
+    Correct the street name based on the type of errors.
+    '''
+    if name.split(' ')[0] != 'Jalan':
+        if name.split(' ')[0] in mapping_street.keys():
+            return name.replace(name.split(' ')[0],mapping_street[name.split(' ')[0]])
+        elif len(name.split(',')) > 1 and name.split(',')[1][:4] == ' JL.':
+            name_list = name.split(',')
+            return name_list[1].replace(' JL.','Jalan') + ' ' + name_list[0]
+        elif name.split(' ')[0][:3] == 'Jl.':
+            return name.replace('Jl.','Jalan ')
+        elif len(name.split(',')) > 1:
+            if name.split(',')[0][:5] == 'Jalan':
+                return name.split(',')[0][:-1]
+            else:
+                return 'Jalan '+ name.split(',')[0][:-1]
+        elif name[-4:] == 'Road':
+            return 'Jalan '+ name[:-5]
+        else:
+            return 'Jalan '+ name
+    else:
+        return name
+
+def audit(osmfile):
+    '''
+    Audit and fix the street name.
+    '''
+    osm_file = open(osmfile, "r")
+    street_types = defaultdict(set)
+    for event, elem in ET.iterparse(osm_file, events=("start",)):
+        if elem.tag == "node" or elem.tag == "way":
+            for tag in elem.iter("tag"):
+                if is_street_name(tag):
+                    audit_street_type(street_types, tag.attrib['v'])
+                    # update tag.attrib['v'] with the return from update_name()
+                    tag.attrib['v'] = update_name(tag.attrib['v'],mapping_street)
+
+    osm_file.close()
+    return street_types
+
+def test():
+    '''
+    Check if the code properly update the street name in a few examples.
+    '''
+    st_types = audit(OSMFILE)
+    pprint.pprint(dict(st_types))
+    for st_type, ways in st_types.iteritems():
+        for name in ways:
+            better_name = update_name(name, mapping_street)
+            if name == "Jln. Danau Poso":
+                assert better_name == "Jalan Danau Poso"
+            if name == "Jl.Gatot Subroto":
+                assert better_name == "Jalan Gatot Subroto"
+            if name == "Raya Kerobokan":
+                assert better_name == "Jalan Raya Kerobokan"
+            if name == "Jalan Bedugul Sidakarya, Denpasar - Bali":
+                assert better_name == "Jalan Bedugul Sidakarya"
+            if name == "tegalsari 37 pantai berawa ,Canggu":
+                print(better_name)
+                assert better_name == "Jalan tegalsari 37 pantai berawa"
+            if name == "Hanoman Road":
+                assert better_name == "Jalan Hanoman"
+            if name == "Komplek Burung, JL. Elang":
+                assert better_name == "Jalan Elang Komplek Burung"
---
+++ explore_osm_data_structure.py
@@ -0,0 +1,79 @@
+'''
+1. Use the iterative parsing to process the map file.
+2. Identify what tags and subtags are in the file, as well as how many they are.
+3. Identify the attributes of child and lower level elements
+4. Return a dictionary with the tag name as the key
+   and the number of times this tag is encountered in the map as the value.
+'''
+import csv
+import codecs
+import re
+import xml.etree.cElementTree as ET
+import pprint
+import cerberus
+import schema
+from collections import Counter,defaultdict
+OSMFILE = 'bali.osm'
+
+def count_tags(elem):
+    '''
+    For each input element, return a dict with the name of tags
+    in the lower level elements as keys and frequency as values.
+    '''
+    tag_dict = {}
+    for sub_elem in elem:
+        tag_dict[sub_elem.tag] = tag_dict.get(sub_elem.tag,0) + 1
+    return tag_dict
+
+def elem_attrib(elem):
+    '''
+    For each input element, return a dict with the name of tags
+    in the lower level elements as keys and attributes as values.
+    '''
+    attrib_dict = {}
+    for sub_elem in elem:
+        if sub_elem.tag not in attrib_dict:
+            attrib_dict[sub_elem.tag] = sub_elem.attrib.keys()
+        else:
+            for attrib in sub_elem.attrib:
+                if attrib not in attrib_dict[sub_elem.tag]:
+                    attrib_dict[sub_elem.tag].append(attrib)
+    return attrib_dict
+
+def merge_dict(dict1,dict2):
+    '''
+    Reference: this code was copied and modified from
+    https://stackoverflow.com/questions/1495510/
+    combining-dictionaries-of-lists-in-python
+    '''
+    keys = set(dict1).union(dict2)
+    no = []
+    return dict((key,list(set(dict1.get(key,no)+dict2.get(key,no)))) for key in keys)
+
+def explore():
+    tree = ET.parse(OSMFILE)
+    root = tree.getroot()
+    child_tags = count_tags(root)
+
+    print('Tags and their frequency in the child elements:')
+    pprint.pprint(child_tags)
+
+    print('\nTags and attributes of the child elements:')
+    child_attrib = elem_attrib(root)
+    pprint.pprint(child_attrib)
+
+    sub_tags, tag_attrib, sub_tags_attrib = {}, {}, {}
+    for key in child_tags.keys():
+        sub_tags[key] = Counter({})
+        sub_tags_attrib[key] = {}
+
+    for child in root:
+        if child.iter():
+            sub_tags[child.tag] += Counter(count_tags(child))
+            sub_tags_attrib[child.tag] = merge_dict(sub_tags_attrib[child.tag],elem_attrib(child))
+
+    print('\nDictionary of tags and their frequency in the lower level elements:')
+    pprint.pprint(sub_tags)
+
+    print('\nDictionary of tags and their attributes in the lower level elements:')
+    pprint.pprint(sub_tags_attrib)
